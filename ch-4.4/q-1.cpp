c44af8e7f6ff76ef0e0ba1d96c0dd879e9482678
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2026-01-02 15:10:02 +0530

    Add files via upload

--- ch-4.4/q-1.cpp
+++ ch-4.4/q-1.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 using namespace std;
 
-class Student{
+class Student
+{
 
 private:
     int ROLL;
@@ -11,78 +12,108 @@ private:
     string NAME;
 
 public:
-    Student(){
+    Student()
+    {
         ROLL = 0;
         GPA = 0;
         NAME = "not set";
     }
 
-    Student (int R, float G, string N){
+    Student(int R, float G, string N)
+    {
         ROLL = R;
         GPA = G;
         NAME = N;
     }
 
-    Student ( const Student &s){
+    Student(const Student &s)
+    {
         ROLL = s.ROLL;
         GPA = s.GPA;
         NAME = s.NAME;
     }
 
-    int gatROLL() const{
+    int gatROLL() const
+    {
         return ROLL;
     };
 
-     float gatGPA() const{
+    float gatGPA() const
+    {
         return GPA;
     };
 
-     string gatNAME() const{
+    string gatNAME() const
+    {
         return NAME;
     };
 
-    void display()const{
+    void display() const
+    {
         cout << "Name :" << NAME << "  Roll no : " << ROLL << "  Gpa :" << GPA << endl;
     }
 
-     ~Student(){
+    ~Student()
+    {
         cout << "\ndestructure called  " << endl;
     }
 };
 
-class StudentManagement{
+class StudentManagement
+{
 
-    private:
-    vector<Student>student;
+private:
+    vector<Student> student;
 
-    public:
-    void addStudent(const Student &s){
+public:
+    void addStudent(const Student &s)
+    {
         student.push_back(s);
         cout << "student Data add";
     }
 
-    void displayAllData(){
+    void displayAllData()
+    {
         cout << "all student detail " << endl;
 
-        if (student.empty()){
+        if (student.empty())
+        {
             cout << "No Student data" << endl;
         }
 
-        else{
-            for (int i = 0; i < student.size(); i++){
+        else
+        {
+            for (int i = 0; i < student.size(); i++)
+            {
                 student[i].display();
+            }
+        }
+    }
+
+    void searchByRoll(int roll) const
+    {
+        for (int i = 0; i < student.size(); i++)
+        {
+            if (student[i].gatROLL() == roll)
+            {
+                cout << "Student Found:\n";
+                student[i].display();
+                return;
+            }
+            else
+            {
+                cout << "Student not found" << endl;
             }
         }
     }
 };
 
+int main()
+{
 
-
-int main(){
-
-    Student s1( 1, 7.11, "Sunil" );
+    Student s1(1, 7.11, "Sunil");
     Student s2(2, 8.50, "Abhay");
-    Student s3(3,9.00, "Rajveer ");
+    Student s3(3, 9.00, "Rajveer ");
 
     StudentManagement manager;
 
@@ -91,6 +122,8 @@ int main(){
     manager.addStudent(s3);
 
     manager.displayAllData();
+
+    manager.searchByRoll(2);
 
     return 0;
 }
